6e71e1241e8b7874dae4191ae2b37d522dfa5dbd
rennq <295570265@qq.com>
2017-11-19 15:22:25 +0800

    添加“demo”module项目

---
+++ demo/src/main/java/org/rennq/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.rennq.demo;
+
+/**
+ * Created by Naz on 2017/11/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World !");
+    }
+}
